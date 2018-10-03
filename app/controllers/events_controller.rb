54ebb84fb42d1e1c6a047e68265c961b50a1c821
Cess <cessmbuguar@gmail.com>
2018-10-03 13:43:30 +0300

    add recurrence to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -50,7 +50,7 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:title, :description,
-      :start_date, :end_date, :organisation_id, :address)
+      :start_date, :end_date, :organisation_id, :address, :occur)
   end
 
   def logged_in_user
